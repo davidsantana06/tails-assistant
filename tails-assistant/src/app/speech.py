8b3d35b3d55a86f4937cea095f3529f9606c48cb
davidsantana06 <davidsantana06@hotmail.com>
2023-10-12 15:25:36 -0300

    refactor: remove try/except from speech package

--- tails-assistant/src/app/speech.py
+++ tails-assistant/src/app/speech.py
@@ -6,32 +6,12 @@ SPEECH_INPUT_TIMEOUT = 5
 
 
 def speech_input(recognizer: Recognizer):
-    speech = None
-
     with Microphone() as audio_source:
         recognizer.adjust_for_ambient_noise(audio_source)
-        print('Fale alguma coisa...')
-
-        try:
-            speech = recognizer.listen(
-                audio_source, timeout=SPEECH_INPUT_TIMEOUT
-            )
-        except:
-            # print_error('Não foi possível ouvir o que você disse.')
-            pass
+        speech = recognizer.listen(audio_source, timeout=SPEECH_INPUT_TIMEOUT)
 
     return speech
 
 
 def speech_transcription(speech: AudioData, recognizer: Recognizer):
-    transcription = None
-
-    try:
-        transcription = recognizer.recognize_google(
-            speech, language=SPEECH_LANGUAGE
-        )
-    except:
-        # print_error('Não foi possível ouvir o que você disse.')
-        pass
-
-    return transcription
+    return recognizer.recognize_google(speech, language=SPEECH_LANGUAGE)
