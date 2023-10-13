2f794141fa920c84b9a141631565dd161a03e805
davidsantana06 <davidsantana06@hotmail.com>
2023-10-13 14:12:41 -0300

    feat: add command execution

--- tails-assistant/src/app/command.py
+++ tails-assistant/src/app/command.py
@@ -1,17 +1,35 @@
+from types import MethodType
+from .actuators import Actuator
+
+
 def validate_command(tokens: list[str], ai_name: str, actions: list[dict[str, object]]):
     valid_command = False
-    action, obj = None, None
+    action, item, method = None, None, None
 
     if (len(tokens) >= 3) and (ai_name == tokens[0]):
-        action, obj = tokens[1], tokens[2]
+        action, item = tokens[1], tokens[2]
 
         for action_entry in actions:
-            if (action == action_entry.get('name')) and (obj in action_entry.get('objects')):
+            if (action in action_entry.get('names')) and (item == action_entry.get('item')):
+                method = action_entry.get('method')
                 valid_command = True
                 break
 
-    return (valid_command, action, obj)
+    return (valid_command, item, method)
 
 
-def execute_command(action: str, obj: str):
-    pass
+def execute_command(item: str, method: str, actuators: tuple[Actuator, ...]):
+    msg, cat = None, None
+
+    for actuator in actuators:
+        if not item in actuator.item_names:
+            continue
+
+        if hasattr(actuator, method):
+            actuator_method = getattr(actuator, method)
+
+            if isinstance(actuator_method, MethodType):
+                msg, cat = actuator_method()
+                break
+
+    return (msg, cat)
